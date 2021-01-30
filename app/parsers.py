a06defa6a4552928248c930211e4ff3d3642735d
physics-machinelearning <kuroki.ryo.physics@gmail.com>
2021-01-30 12:36:52 +0900

    add argparse

--- app/parsers.py
+++ app/parsers.py
@@ -1,7 +1,10 @@
+import argparse
+
 import pandas as pd
 import arxiv
 
 from db_tools import InteractArticle
+from config import START, END, YESTARDAY, TODAY, CATEGORIES
 
 
 def article_parser(START, END, categories):
@@ -27,3 +30,16 @@ def _article_parser_category(START, END, category):
     result = arxiv.query(query=query, sort_by='submittedDate')
     result_df = pd.io.json.json_normalize(result)
     return result_df
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_artument('arg1')
+    args = parser.parse_args()
+
+    if args.arg1 == 'all':
+        article_parser(START, END, CATEGORIES)
+    elif args.arg1 == 'today':
+        article_parser(YESTARDAY, TODAY, CATEGORIES)
+    else:
+        print('wrong argument')
