7730fd201ea380b392ba65827703110d04cf1189
physics-machinelearning <kuroki.ryo.physics@gmail.com>
2021-02-07 15:00:21 +0900

    set register to default page

--- arxivapp/views.py
+++ arxivapp/views.py
@@ -33,7 +33,10 @@ def logout_view(request):
 
 def register(request):
     if request.method == 'GET':
-        return render(request, 'register.html')
+        if request.user.is_authenticated:
+            return redirect('category')
+        else:
+            return render(request, 'register.html')
     elif request.method == 'POST':
         username = request.POST.get('username')
         email = request.POST.get('email')
